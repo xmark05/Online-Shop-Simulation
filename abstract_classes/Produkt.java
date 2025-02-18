e9542e08b260c087bed57ee2215e9a2ea0f183d6
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-02-18 11:01:25 +0100

    getter/setter created and a constructor

--- abstract_classes/Produkt.java
+++ abstract_classes/Produkt.java
@@ -1,5 +1,41 @@
 package abstract_classes;
+
 public abstract class Produkt {
 
+    private int preis;
+    private String name;
+    private String kategorie;
+
+    public Produkt(int preis, String name, String kategorie) {
+        this.preis = preis;
+        this.name = name;
+        this.kategorie = kategorie;
+    }
+
+    public int getPreis() {
+        return preis;
+    }
+
+    public void setPreis(int preis) {
+        this.preis = preis;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKategorie() {
+        return kategorie;
+    }
+
+    public void setKategorie(String kategorie) {
+        this.kategorie = kategorie;
+    }
     
+
+    public abstract double berechneRabatt();
 }
