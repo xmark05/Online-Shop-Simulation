fad571d68a6a3c6b40f5a8d06461dd8ce0e5ae93
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-03-18 09:55:44 +0100

    Konstruktor gebaut und Rabatt gegeben

--- subclasses/Buch.java
+++ subclasses/Buch.java
@@ -1,5 +1,20 @@
 package subclasses;
-public class Buch {
+
+import Interfaces.IRabattierbar;
+import abstract_classes.Produkt;
+
+public class Buch extends Produkt implements IRabattierbar {
+
+    public Buch(int preis, String name, String kategorie) {
+        super(preis, name, kategorie);
+    }
+
+    @Override
+    public double berechneRabatt() {
+        return getPreis() * 0.90; // 10% Rabatt
+    }
+
+    
 
     
 }
--- subclasses/Elektronik.java
+++ subclasses/Elektronik.java
@@ -1,5 +1,18 @@
 package subclasses;
-public class Elektronik {
+
+import Interfaces.IRabattierbar;
+import abstract_classes.Produkt;
+
+public class Elektronik extends Produkt implements IRabattierbar{
+
+    public Elektronik(int preis, String name, String kategorie) {
+        super(preis, name, kategorie);
+    }
+    
+    @Override
+    public double berechneRabatt() {
+        return getPreis() * 0.80; // 20% Rabatt
+    }
 
     
 }
