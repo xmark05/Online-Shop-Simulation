05bcf9ecd9222934dbb74b91299ed567e7a9f470
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-03-18 09:53:43 +0100

    Konstruktor gebaut und 5% Rabatt gegeben

--- subclasses/Kleidung.java
+++ subclasses/Kleidung.java
@@ -1,5 +1,19 @@
 package subclasses;
-public class Kleidung {
+
+import Interfaces.IRabattierbar;
+import abstract_classes.Produkt;
+
+public class Kleidung extends Produkt implements IRabattierbar {
+
+    public Kleidung(int preis, String name, String kategorie){
+        super(preis,name,kategorie);
+    }
+
+    @Override
+    public double berechneRabatt() {
+        return getPreis() * 0.95; // 5% Rabatt
+        
+    }
 
     
 }
