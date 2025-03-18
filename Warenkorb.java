a62dac8281be7fea2a82994084e4f3a5e7a77f9f
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-03-18 10:13:51 +0100

    Methoden hinzugefüt

--- Warenkorb.java
+++ Warenkorb.java
@@ -1,6 +1,26 @@
 public class Warenkorb {
 
     public static void main(String[] args) {
+
+
+
         
+        
+    }
+
+    static void fuegeHinzu(){
+
+    }
+
+    static void entferneProdukt(){
+
+    }
+
+    double berechneGesamtpreis(){
+        return 0;
+    }
+
+    void zeigeProdukte(){ 
+
     }
 }
